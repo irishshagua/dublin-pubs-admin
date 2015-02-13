1ea45f2230a36f54ed4efecc086120e7aeb7329e
Brian Mooney <brian_satchwannabe@hotmail.com>
2015-02-13 22:36:48 +0000

    Localize the UI strings
    Add Chinese version of the strings

--- src/main/java/com/mooneyserver/dublinpubs/util/resource/ResourceAccessor.java
+++ src/main/java/com/mooneyserver/dublinpubs/util/resource/ResourceAccessor.java
@@ -1,29 +1,30 @@
 package com.mooneyserver.dublinpubs.util.resource;
 
 import java.net.URL;
+import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class ResourceAccessor {
 
 	// FXML Files
 	public static final String FXML_LANDING_PAGE = "fxml/MainFrame.fxml";
-	
+
 	// CSS Files
 	public static final String CSS_APP = "css/application.css";
-	
+
 	// Localized Strings
 	public static final String STRINGS = "i18n/AdminStrings";
-	
+
 	// Localization Keys
 	public static final String APP_FRAME_TITLE = "app.frame.title";
 	public static final String APP_TABS_PUBLIST_PUBCOUNT = "app.tabs.pub_list.pub_count_lbl";
-	
-	
+
 	public URL getResource(String resourceName) {
-		return ResourceAccessor.class.getClassLoader().getResource(resourceName);
+		return ResourceAccessor.class.getClassLoader()
+				.getResource(resourceName);
 	}
-	
+
 	public ResourceBundle getResourceBundle() {
-		return ResourceBundle.getBundle(STRINGS);
+		return ResourceBundle.getBundle(STRINGS, Locale.getDefault());
 	}
 }
