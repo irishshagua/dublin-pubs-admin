00fa2243d206a2b86955ba446e8ea54aee5bd385
Brian Mooney <brian_satchwannabe@hotmail.com>
2015-03-03 23:22:42 +0000

    Inject Rest Client

--- src/main/java/com/mooneyserver/dublinpubs/service/PubService.java
+++ src/main/java/com/mooneyserver/dublinpubs/service/PubService.java
@@ -1,11 +1,14 @@
 package com.mooneyserver.dublinpubs.service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
-import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+import javax.ws.rs.ProcessingException;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.ResponseProcessingException;
 import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.MediaType;
 
@@ -22,12 +25,8 @@ public class PubService {
 	private static final String REST_BASE_URL = "http://dublinpubs-mooneyserver.rhcloud.com/rest";
 	private static final String REST_PUBS_PATH = "pubs";
 
+	@Inject
 	private Client restClient;
-
-	@PostConstruct
-	void initRestClient() {
-		restClient = ClientBuilder.newClient();
-	}
 
 	public CompletableFuture<List<Pub>> retrieveVisitedPubsAsync() {
 		return CompletableFuture.supplyAsync(this::retrieveVisitedPubs);
@@ -35,10 +34,25 @@ public class PubService {
 
 	public List<Pub> retrieveVisitedPubs() {
 		LOGGER.debug("Starting call to the 'Visited Pubs' REST service now");
-		List<Pub> visitedPubs = restClient.target(REST_BASE_URL)
-				.path(REST_PUBS_PATH).request(MediaType.APPLICATION_JSON)
-				.get(new GenericType<List<Pub>>() {
-				});
+		List<Pub> visitedPubs = new ArrayList<Pub>();
+
+		try {
+			visitedPubs = restClient
+				.target(REST_BASE_URL)
+				.path(REST_PUBS_PATH)
+				.request(MediaType.APPLICATION_JSON)
+				.get(new GenericType<List<Pub>>() {});
+		} catch (ResponseProcessingException e) {
+			LOGGER.error(
+					"Error retrieving pubs. Unable to deserialize response {}",
+					e.getResponse().getEntity(), e);
+			throw e;
+		} catch (ProcessingException | WebApplicationException e) {
+			LOGGER.error(
+					"Error retrieving pubs. Rest Call to Pubs endpoint failed",
+					e);
+			throw e;
+		}
 		LOGGER.debug("'Visited Pubs' Rest service call complete: {}",
 				visitedPubs);
 
--- src/main/java/com/mooneyserver/dublinpubs/util/di/JavaFxRelatedProducers.java
+++ src/main/java/com/mooneyserver/dublinpubs/util/di/JavaFxRelatedProducers.java
@@ -1,11 +1,15 @@
 package com.mooneyserver.dublinpubs.util.di;
 
 import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeUnit;
 
 import javafx.fxml.FXMLLoader;
 
 import javax.enterprise.inject.Produces;
 import javax.inject.Inject;
+import javax.ws.rs.client.Client;
+
+import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
 
 public class JavaFxRelatedProducers {
 
@@ -14,6 +18,13 @@ public class JavaFxRelatedProducers {
 
 	@Produces
 	public FXMLLoader createLoader() {
-		return new FXMLLoader(null, null, null, fxmlLoaderCallback, StandardCharsets.UTF_8);
+		return new FXMLLoader(null, null, null, fxmlLoaderCallback,
+				StandardCharsets.UTF_8);
+	}
+
+	@Produces
+	public Client createRestClient() {
+		return new ResteasyClientBuilder().socketTimeout(15, TimeUnit.SECONDS)
+				.build();
 	}
 }
